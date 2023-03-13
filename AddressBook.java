6494dae9ca70810d047d06e8c5ac590ee6f55e4b
SUDHEER341 <sudheer601220@gmail.com>
2023-03-13 14:52:08 +0530

    Added : Edit  the details of a person as per user requirement

--- AddressBook.java
+++ AddressBook.java
@@ -1,123 +1,80 @@
 package com.addressbook;
+
 import java.util.ArrayList;
 import java.util.Scanner;
-
-//Address book program
 public class AddressBook extends PersonDetails {
-    public static AddressBook newPerson = new AddressBook();
-    public static ArrayList<PersonDetails> person = new ArrayList<>();
-    public static void addPerson(){
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter first name");
-        newPerson.setFirstName(scanner.nextLine());
-        System.out.println("Enter last name");
-        newPerson.setLastName(scanner.nextLine());
-        System.out.println("Enter address");
-        newPerson.setAddress(scanner.nextLine());
-        System.out.println("Enter city");
-        newPerson.setCity(scanner.nextLine());
-        System.out.println("Enter state");
-        newPerson.setState(scanner.nextLine());
-        System.out.println("Enter zip code");
-        newPerson.setZip(scanner.nextLine());
-        System.out.println("Enter phone number");
-        newPerson.setPhoneNumber(scanner.nextLine());
-        System.out.println("Enter Email");
-        newPerson.setEmail(scanner.nextLine());
-
-        PersonDetails contactPerson = new PersonDetails(newPerson.getFirstName(),newPerson.getLastName(), newPerson.getAddress(), newPerson.getCity(), newPerson.getState(),
-                                         newPerson.getZip(), newPerson.getPhoneNumber(),newPerson.getEmail());
-                                            person.add(contactPerson);
-        System.out.println(person);
-    }
-
-    public static void deletePerson(){
-        Scanner scanner = new Scanner(System.in);
-        boolean found=false;
-        System.out.println("Enter the first name of contact to delete");
-        String nameDelete;
-        nameDelete = scanner.nextLine();
-        ListIterator<PersonDetails> iterator = person.listIterator();
-        while (iterator.hasNext()) {
-            PersonDetails contact = iterator.next();
-            if (nameDelete.equals(contact.getFirstName())) {
-                iterator.remove();
-                found=true;
-            }
-        }
-        if (found){
-            System.out.println("record is deleted");
-            System.out.println(person);
-        }else {
-            System.out.println("record not found");
-        }
-    }
-    public static void modifyUser()
-
-    {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the first name whose contact to be edited");
-        String name = scanner.nextLine();
-        boolean found=false;
-        ListIterator<PersonDetails> iterator = person.listIterator();
-        while (iterator.hasNext()){
-            PersonDetails contact= iterator.next();
-            if (name.equals(contact.getFirstName()))
-            {
-                System.out.println("Edit first name");
-                newPerson.setFirstName(scanner.nextLine());
-                System.out.println("Edit last name");
-                newPerson.setLastName(scanner.nextLine());
-                System.out.println("Edit address");
-                newPerson.setAddress(scanner.nextLine());
-                System.out.println("Edit city");
-                newPerson.setCity(scanner.nextLine());
-                System.out.println("Edit state");
-                newPerson.setState(scanner.nextLine());
-                System.out.println("Edit zip code");
-                newPerson.setZip(scanner.nextLine());
-                System.out.println("Edit phone number");
-                newPerson.setPhoneNumber(scanner.nextLine());
-                System.out.println("Edit Email");
-                newPerson.setEmail(scanner.nextLine());
-                iterator.set(new PersonDetails(newPerson.getFirstName(),newPerson.getLastName(), newPerson.getAddress(), newPerson.getCity(), newPerson.getState(), newPerson.getZip(), newPerson.getPhoneNumber(),newPerson.getEmail()));
-                found=true;
-            }
-
-        }
-        if (found){
-            System.out.println("record is update");
-            System.out.println(person);
-        }else {
-            System.out.println("record not found");
-        }
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         int flag = 0;
-        int choice;
-        System.out.println("Welcome to Address Book Program");
+        int option;
+
+        AddressBook newPerson = new AddressBook();
+        ArrayList<AddressBook> person = new ArrayList<>();
+        System.out.println("Welcome to Address Book System Programm");
+        Scanner scanner = new Scanner(System.in);
 
         while (flag != 1) {
-
-            System.out.println("Enter 1 to add contact details");
-            System.out.println("Enter 2 to edit contact details");
-            System.out.println("Enter 3 to delete contact");
-            System.out.println("Enter 4 to Exit from the Adressbook");
-
-            choice = scanner.nextInt();
+            System.out.println("Press 1 to add contact details of a person");
+            System.out.println("Press 2 to edit details in the addressbook");
+            System.out.println("Press 3 to exit from the addresbook");
+            option = scanner.nextInt();
             scanner.nextLine();
 
+            if (option == 1) {
 
-            if (choice == 1) {
-                addPerson();
-            } else if (choice == 2) {
-                modifyUser();
-            } else if (choice==3) {
-                deletePerson();
-            } else if (choice == 4) {
+                System.out.println("Enter first name");
+                newPerson.setFirstName(scanner.nextLine());
+                System.out.println("Enter last name");
+                newPerson.setLastName(scanner.nextLine());
+                System.out.println("Enter address");
+                newPerson.setAddress(scanner.nextLine());
+                System.out.println("Enter city");
+                newPerson.setCity(scanner.nextLine());
+                System.out.println("Enter state");
+                newPerson.setState(scanner.nextLine());
+                System.out.println("Enter zip code");
+                newPerson.setZip(scanner.nextInt());
+                System.out.println("Enter phone number");
+                newPerson.setPhoneNumber(scanner.nextInt());
+                System.out.println("Enter Email");
+                scanner.nextLine();
+                newPerson.setEmail(scanner.nextLine());
+                person.add(newPerson);
+
+                System.out.println(person);
+            }
+
+            else if (option == 2)
+            {
+                System.out.println("Enter the first name whose contact to be edited");
+                String name = scanner.nextLine();
+                if (name.equals(newPerson.getFirstName()))
+                {
+                    System.out.println("Edit first name");
+                    newPerson.setFirstName(scanner.nextLine());
+                    System.out.println("Edit last name");
+                    newPerson.setLastName(scanner.nextLine());
+                    System.out.println("Edit address");
+                    newPerson.setAddress(scanner.nextLine());
+                    System.out.println("Edit city");
+                    newPerson.setCity(scanner.nextLine());
+                    System.out.println("Edit state");
+                    newPerson.setState(scanner.nextLine());
+                    System.out.println("Edit zip code");
+                    newPerson.setZip(scanner.nextInt());
+                    System.out.println("Edit phone number");
+                    newPerson.setPhoneNumber(scanner.nextInt());
+                    scanner.nextLine();
+                    System.out.println("Edit Email Address");
+                    newPerson.setEmail(scanner.nextLine());
+                    person.add(newPerson);
+                }
+                else
+                {
+                    System.out.println("Given name is  doesn't exist in the addressbook");
+                }
+            }
+            else if (option == 3)
+            {
                 flag = 1;
             }
         }
--- PersonDetails.java
+++ PersonDetails.java
@@ -1,74 +1,59 @@
-package com.addressbook;public class PersonDetails {
+package com.addressbook;
+public class PersonDetails {
 
     String firstName = new String();
     String lastName = new String();
     String address = new String();
     String city = new String();
     String state = new String();
-    int zip;
-    long phoneNumber;
+    String zip;
+    String  phoneNumber;
     String email = new String();
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
-    public long getPhoneNumber() {
+    public String getPhoneNumber() {
         return phoneNumber;
     }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
-    public int getZip() {
+    public String getZip() {
         return zip;
     }
-
     public void setZip(String zip) {
         this.zip = zip;
     }
-
     public String getState() {
         return state;
     }
-
     public void setState(String state) {
         this.state = state;
     }
-
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
     public String getCity() {
         return city;
     }
-
     public void setCity(String city) {
         this.city = city;
     }
-
-
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getFirstName() {
         return firstName;
     }
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getLastName() {
         return lastName;
     }
@@ -80,7 +65,6 @@ package com.addressbook;public class PersonDetails {
         this.state=state;
         this.zip=zip;
         this.phoneNumber=phoneNumber;
-
     }
     public PersonDetails(){
         super();
@@ -88,8 +72,8 @@ package com.addressbook;public class PersonDetails {
     @Override
     public String toString() {
         return "Employee [name=" + this.firstName +" "+ this.lastName + ", " +
-                "Address=" + this.address + ", City="
-                + this.city + ", state=" + this.state + ", zip=" + this.zip
-                + ", phone=" + this.phoneNumber + "]";
+                                        "Address=" + this.address + ", City="
+                            + this.city + ", state=" + this.state + ", zip=" + this.zip
+                                     + ", phone=" + this.phoneNumber + "]";
     }
 }
